9db2c72981247079c858191cb52774143809a76e
bestaford <butenko1demitr@gmail.com>
2023-09-22 15:39:37 +0300

    "/about" command

--- src/main/java/ru/bestaford/bstorage/BStorageBot.java
+++ src/main/java/ru/bestaford/bstorage/BStorageBot.java
@@ -24,6 +24,7 @@ import java.util.*;
 
 public class BStorageBot {
 
+    public static final String VERSION = "1.0.2";
     public static final String JDBC_URL = "jdbc:h2:./bstorage";
 
     public final Logger logger;
@@ -80,6 +81,7 @@ public class BStorageBot {
             if (text.startsWith("/")) {
                 switch (text.substring(1)) {
                     case "start", "help" -> sendHelp(user);
+                    case "about" -> sendMessage(user, String.format(messages.getString("about"), VERSION));
                     default -> sendMessage(user, messages.getString("command.unknown"));
                 }
             } else {
