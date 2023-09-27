09f089258e087c4954c55ab4228018dcb1077285
bestaford <butenko1demitr@gmail.com>
2023-09-27 14:57:06 +0300

    Show supported file types in /help

--- src/main/java/ru/bestaford/bstorage/command/HelpCommand.java
+++ src/main/java/ru/bestaford/bstorage/command/HelpCommand.java
@@ -2,6 +2,7 @@ package ru.bestaford.bstorage.command;
 
 import com.pengrad.telegrambot.model.User;
 import ru.bestaford.bstorage.BStorageBot;
+import ru.bestaford.bstorage.model.File;
 
 public final class HelpCommand extends Command {
 
@@ -11,6 +12,15 @@ public final class HelpCommand extends Command {
 
     @Override
     public void execute(User user) {
-        bot.sendMessage(user, String.format(bot.messages.getString("help"), bot.me.username()));
+        StringBuilder result = new StringBuilder(String.format(bot.messages.getString("help"), bot.me.username()));
+        result.append("\n\n");
+        for (File.Type type : File.Type.values()) {
+            String typeString = type.toString();
+            result.append("<b>•</b> ");
+            result.append(typeString.substring(0, 1).toUpperCase());
+            result.append(typeString.substring(1).toLowerCase());
+            result.append("\n");
+        }
+        bot.sendMessage(user, result.toString());
     }
 }
