78a46446f791a19a4dd29237546c8fd64fdecf95
Bestaford <butenko1demitr@gmail.com>
2023-11-30 13:26:42 +0300

    v1.1.2

--- src/main/java/ru/bestaford/bstorage/BStorageBot.java
+++ src/main/java/ru/bestaford/bstorage/BStorageBot.java
@@ -23,7 +23,7 @@ import java.util.*;
 
 public final class BStorageBot {
 
-    public static final String VERSION = "1.1.1";
+    public static final String VERSION = "1.1.2";
 
     public static final String JDBC_URL = "jdbc:h2:./bstorage";
     public static final String JDBC_USER = "";
