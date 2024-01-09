df8b85b0d669ec3f6762dc4d3abfa91b917408fe
Bestaford <butenko1demitr@gmail.com>
2024-01-09 12:24:34 +0300

    v1.1.3

--- src/main/java/ru/bestaford/bstorage/BStorageBot.java
+++ src/main/java/ru/bestaford/bstorage/BStorageBot.java
@@ -23,7 +23,7 @@ import java.util.*;
 
 public final class BStorageBot extends TelegramBot {
 
-    public static final String VERSION = "1.1.2";
+    public static final String VERSION = "1.1.3";
 
     public static final String JDBC_URL = "jdbc:h2:./bstorage";
     public static final String JDBC_USER = "";
